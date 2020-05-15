b4587b3a972d26deccc8d3b222206ccb84474241
Jordan <hello@jordan-massart.be>
2020-05-15 18:55:33 +0200

    add users and team helpers method

--- app/HasTeams.php
+++ app/HasTeams.php
@@ -5,7 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-trait HasTeams {
+trait HasTeams
+{
     public function teams(): BelongsToMany
     {
         return $this->belongsToMany(Team::class)->withTimestamps();
@@ -14,5 +15,28 @@ trait HasTeams {
     public function currentTeam(): HasOne
     {
         return $this->hasOne(Team::class, 'id', 'current_team_id');
+    }
+
+    public function isOwner()
+    {
+        return ($this->teams()->where('owner_id', $this->getKey())->first()) ? true : false;
+    }
+
+    public function isOwnerOfTeam($team)
+    {
+        $team_id = $this->retrieveTeamId($team);
+        return ($this->teams()->where('owner_id', $this->getKey())->where('team_id', $team_id)->first()) ? true : false;
+    }
+
+    public function retrieveTeamId($team)
+    {
+        if(is_object($team)) {
+            $team = $team->getKey();
+        }
+        if(is_array($team) && isset($team['id'])) {
+            $team = $team['id'];
+        }
+
+        return $team;
     }
 }
