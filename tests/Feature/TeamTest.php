c646d58ce95828fccdbc8bd10d44aa645fa5b3d4
Jordan <hello@jordan-massart.be>
2020-05-15 18:56:44 +0200

    Add authorization

--- tests/Feature/TeamTest.php
+++ tests/Feature/TeamTest.php
@@ -6,7 +6,11 @@ use App\Mail\InviteToTeam;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
 use Livewire\Livewire;
 use Tests\TestCase;
 
@@ -29,14 +33,48 @@ class TeamTest extends TestCase
 
         Livewire::actingAs($user)
             ->test('teams.home')
-            ->set('ownerId', $user->id)
-            ->set('name', 'Foo Bar Baz')
+            ->set('teamName', 'Foo Bar Baz')
             ->call('store');
 
         $user->refresh();
 
         $this->assertEquals('Foo Bar Baz', $user->currentTeam->name);
         $this->assertEquals($user->id, $user->currentTeam->owner_id);
+    }
+
+    /** @test */
+    public function the_team_name_is_required()
+    {
+        $user = factory(User::class)->create();
+
+        Livewire::actingAs($user)
+            ->test('teams.home')
+            ->call('store')
+            ->assertHasErrors(['teamName' => 'required']);
+    }
+
+    /** @test */
+    public function the_team_name_has_min_characters()
+    {
+        $user = factory(User::class)->create();
+
+        Livewire::actingAs($user)
+            ->test('teams.home')
+            ->set('teamName', 'a')
+            ->call('store')
+            ->assertHasErrors(['teamName' => 'min']);
+    }
+
+    /** @test */
+    public function the_team_name_has_max_characters()
+    {
+        $user = factory(User::class)->create();
+
+        Livewire::actingAs($user)
+            ->test('teams.home')
+            ->set('teamName', Str::random(25))
+            ->call('store')
+            ->assertHasErrors(['teamName' => 'max']);
     }
 
     /** @test */
@@ -52,5 +90,51 @@ class TeamTest extends TestCase
             ->call('invite');
 
         Mail::assertQueued(InviteToTeam::class);
+    }
+
+    /** @test */
+    public function a_new_member_is_added_to_the_group_once_he_accept_the_invitation()
+    {
+        $this->markTestIncomplete('Find a way to pass the request url');
+        $user = factory(User::class)->state('hasTeam')->create();
+        $url = URL::temporarySignedRoute('inviteToTeam', now()->addMinutes(30), ['req' => Crypt::encrypt(['email' => 'john@doe.com', 'teamId' => $user->currentTeam->id])]);
+
+        Livewire::test('teams.account')
+            ->set('name', 'Carlos')
+            ->set('password', 'password')
+            ->call('complete', [new Request(['req' => 'abc'])]);
+    }
+
+    /** @test */
+    public function owner_can_remove_team_member()
+    {
+        $owner = factory(User::class)->state('hasTeam')->create();
+        $member = factory(User::class)->create();
+        $member->teams()->attach($owner->currentTeam->id);
+
+        Livewire::actingAs($owner)
+            ->test('teams.home')
+            ->set('memberId', $member->id)
+            ->call('deleteMember');
+
+        $member->refresh();
+
+        $this->assertCount(0, $member->teams);
+    }
+
+    /** @test */
+    public function member_is_not_able_to_remove_another_member()
+    {
+        $owner = factory(User::class)->state('hasTeam')->create();
+        $member = factory(User::class)->create();
+        $member->teams()->attach($owner->currentTeam->id);
+        $memberTryToRemoveMember = factory(User::class)->create(['current_team_id' => $owner->currentTeam->id]);
+        $memberTryToRemoveMember->teams()->attach($owner->currentTeam->id);
+
+        Livewire::actingAs($memberTryToRemoveMember)
+            ->test('teams.home')
+            ->call('deleteMember', $member->id);
+
+        $this->assertCount(0, $member->teams);
     }
 }
