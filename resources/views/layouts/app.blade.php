01bd8ba6f0c12fd86b06631752b5d8dc0fb1ffc0
Jordan <hello@jordan-massart.be>
2020-05-16 12:44:27 +0200

    add fathom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,6 +93,21 @@
     </div>
   </div></div>
 <script src="{{ mix('js/app.js') }}"></script>
+<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+<script>
+    (function(f, a, t, h, o, m){
+        a[h]=a[h]||function(){
+            (a[h].q=a[h].q||[]).push(arguments)
+        };
+        o=f.createElement('script'),
+            m=f.getElementsByTagName('script')[0];
+        o.async=1; o.src=t; o.id='fathom-script';
+        m.parentNode.insertBefore(o,m)
+    })(document, window, '//localhost:8888/tracker.js', 'fathom');
+    fathom('set', 'siteId', 'FCIHE');
+    fathom('trackPageview');
+</script>
+<!-- / Fathom -->
 @livewireScripts
 </body>
 </html>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,6 +11,21 @@
 <body class="bg-gray-50">
 @yield('content')
 <script src="{{ mix('js/app.js') }}"></script>
+<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+<script>
+    (function(f, a, t, h, o, m){
+        a[h]=a[h]||function(){
+            (a[h].q=a[h].q||[]).push(arguments)
+        };
+        o=f.createElement('script'),
+            m=f.getElementsByTagName('script')[0];
+        o.async=1; o.src=t; o.id='fathom-script';
+        m.parentNode.insertBefore(o,m)
+    })(document, window, '//localhost:8888/tracker.js', 'fathom');
+    fathom('set', 'siteId', 'FCIHE');
+    fathom('trackPageview');
+</script>
+<!-- / Fathom -->
 @livewireScripts
 </body>
 </html>
