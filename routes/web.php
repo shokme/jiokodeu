f8f7ff809de16ce8c2bc8e44183225d906e8c989
Jordan <hello@jordan-massart.be>
2020-05-21 13:05:27 +0200

    chore

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::middleware('auth')->group(function () {
     Route::livewire('/dashboard', 'dashboard.home')->name('dashboard');
     Route::livewire('/teams', 'teams.home')->name('teams');
     Route::livewire('/billings', 'dashboard.billing')->name('billings');
+    Route::livewire('/dashboard/activities', 'dashboard.activity')->name('activities');
     Route::get('/billings/checkout', function () {
         /** @var \App\User $user */
         $user = Auth::user();
